82ae5107cc3ec24c002e913b6bd7978ba4983ca4
Boom Tantikarun <bonedz@gmail.com>
2014-05-09 16:17:37 +0200

    Recommender Part

---
+++ python/bookselection.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+
+import BaseXClient
+import xml.dom.minidom as xmldom
+import Classification
+import Tree
+from array import *
+from collections import defaultdict
+from sets import Set
+
+def bookselection(userID, I):
+	try:
+		session1 = BaseXClient.Session('localhost',1984,'admin','admin')
+		session1.execute('open keyword')
+		print session1.info()
+
+		recommend = {}
+		categorylist = Classification.getclassificationrule()
+		for cat in categorylist:
+			recommend[cat] = Set()
+
+		for cat in I:
+			find_ref = '''for $book in //keywordXML/classification/*
+						where $book/../@code="'''+cat+'''"
+						return (data($book/@noticekoha),$book/keyword/text(),'$')'''
+			query_ref = session1.query(find_ref)
+			buff = []
+			for typecode,ref in query_ref.iter():
+				if(buff!='$'):
+					noticekoha = buff[0]
+					for keyword in buff[1:len(buff)-1]:
+						if(I[cat][keyword] > 2):
+							recommend[cat].add(noticekoha)
+					buff = []
+				else:
+					buff.append(ref)
+
+		session1.close()
+
+		return recommend
+	except IOError as e:
+		print e
